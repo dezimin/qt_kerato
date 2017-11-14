fc154ff2c0b1411453487681eb54abda40c954d6
dezimin <vinogradov.vladimir.main@gmail.com>
2017-11-14 17:50:28 +0300

    Добавил карту приложения
    Карта приложения для быстрого поиска и исправления ошибок

---
+++ map_application.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1510670891414" ID="ID_1420314924" MODIFIED="1510670891414" TEXT="&#x41d;&#x43e;&#x432;&#x430;&#x44f; &#x43a;&#x430;&#x440;&#x442;&#x430;"/>
+</map>
