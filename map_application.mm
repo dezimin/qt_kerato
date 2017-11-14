4b038609988de587b02ab63fd81beebb6d7ef4fb
dezimin <vinogradov.vladimir.main@gmail.com>
2017-11-14 18:16:05 +0300

    Добавил предварительную обработку в карту

--- map_application.mm
+++ map_application.mm
@@ -1,4 +1,26 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1510670891414" ID="ID_1420314924" MODIFIED="1510670891414" TEXT="&#x41d;&#x43e;&#x432;&#x430;&#x44f; &#x43a;&#x430;&#x440;&#x442;&#x430;"/>
+<node CREATED="1510670891414" ID="ID_1420314924" MODIFIED="1510671335037" TEXT="">
+<node CREATED="1510670891414" HGAP="9" ID="ID_1741741999" MODIFIED="1510671642795" POSITION="right" TEXT="&#x41c;&#x43e;&#x434;&#x443;&#x43b;&#x44c; &#x43f;&#x440;&#x435;&#x434;&#x432;&#x430;&#x440;&#x438;&#x442;&#x435;&#x43b;&#x44c;&#x43d;&#x43e;&#x439; &#x43e;&#x431;&#x440;&#x430;&#x431;&#x43e;&#x442;&#x43a;&#x438; - Kerato_image_proc" VSHIFT="-133">
+<node CREATED="1510671330056" HGAP="19" ID="ID_64760887" MODIFIED="1510672480164" TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x44f; &#x43f;&#x43e;&#x43b;&#x443;&#x447;&#x435;&#x43d;&#x438;&#x44f; &#x437;&#x43d;&#x430;&#x447;&#x435;&#x43d;&#x438;&#x439; &#x446;&#x435;&#x43d;&#x442;&#x440;&#x430; - calc_center(image_source);" VSHIFT="20">
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1510671404545" ID="ID_88325417" MODIFIED="1510672478267" TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x44f; &#x430;&#x432;&#x442;&#x43e;&#x43c;&#x430;&#x442;&#x438;&#x447;&#x435;&#x441;&#x43a;&#x43e;&#x433;&#x43e; &#x443;&#x434;&#x430;&#x43b;&#x435;&#x43d;&#x438;&#x44f; &#x43e;&#x431;&#x43b;&#x430;&#x441;&#x442;&#x438; - delete_auto_frame(image_source, center);" VSHIFT="9">
+<icon BUILTIN="down"/>
+<node CREATED="1510671451737" HGAP="32" ID="ID_1444636192" MODIFIED="1510671927368" TEXT="delete_frame(image_source);" VSHIFT="6">
+<icon BUILTIN="back"/>
+</node>
+</node>
+<node CREATED="1510671445467" HGAP="21" ID="ID_711002653" MODIFIED="1510672476636" TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x44f; &#x443;&#x434;&#x430;&#x43b;&#x435;&#x43d;&#x438;&#x44f; &#x437;&#x430;&#x441;&#x432;&#x435;&#x442;&#x43e;&#x432; - brightless_delete(image_source);" VSHIFT="5">
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1510672309700" ID="ID_1942172718" MODIFIED="1510672474636" TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x44f; &#x431;&#x438;&#x43d;&#x430;&#x440;&#x438;&#x437;&#x430;&#x446;&#x438;&#x438; &#x43c;&#x435;&#x442;&#x43e;&#x434;&#x43e;&#x43c; OTSU - binar(image_source);" VSHIFT="2">
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1510672422196" HGAP="21" ID="ID_971713561" MODIFIED="1510672511908" TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x44f; &#x43f;&#x43e;&#x43b;&#x443;&#x447;&#x435;&#x43d;&#x438;&#x44f; &#x434;&#x432;&#x443;&#x445; &#x43b;&#x438;&#x43d;&#x438;&#x439; - two_line(image_source)">
+<icon BUILTIN="closed"/>
+</node>
+</node>
+<node CREATED="1510671300057" ID="ID_793410764" MODIFIED="1510671300057" POSITION="left" TEXT=""/>
+</node>
 </map>
